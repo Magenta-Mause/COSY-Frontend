af3d05f6ae88855f9c4fe6b7ddd58a2fc3a1c14e
Janne <jabbekeipert@gmail.com>
2025-11-29 22:19:53 +0100

    add base design

--- src/components/display/GameServerConfiguration/GameServerConfigurationHouse/GameServerConfigurationHouse.tsx
+++ src/components/display/GameServerConfiguration/GameServerConfigurationHouse/GameServerConfigurationHouse.tsx
@@ -1,11 +1,24 @@
 import type {GameServerConfigurationEntity} from "@/api/generated/model";
 import {cn} from "@/lib/utils.ts";
 import type {CSSProperties} from "react";
+import serverHouseImage from "@/assets/house.png";
 
-const GameServerConfigurationHouse = (props: { gameServer: GameServerConfigurationEntity, className?: string, style?: CSSProperties }) => {
-  return <div className={cn("block overflow-hidden w-[200px] h-[200px] border-black border-4 text-xs", props.className)} style={props.style}>
-    {JSON.stringify(props.gameServer)}
-  </div>
+const GameServerConfigurationHouse = (props: {
+    gameServer: GameServerConfigurationEntity,
+    className?: string,
+    style?: CSSProperties
+}) => {
+
+    return <a
+        className={cn("block overflow-hidden w-[10%] h-auto aspect-square border-black border-4 text-xs", props.className)}
+        href={`/game-server-configuration/${props.gameServer.uuid}`}
+        aria-label={`Game Server Configuration: ${props.gameServer.server_name}`}
+        style={props.style}>
+        <img
+            className="w-full h-full object-cover"
+            aria-label={`Game Server Configuration: ${props.gameServer.server_name}`}
+            src={serverHouseImage}/>
+    </a>
 }
 
 export default GameServerConfigurationHouse;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,31 +1,38 @@
 import {createFileRoute} from "@tanstack/react-router";
 import GameServerConfigurationsDisplay
-  from "@components/display/GameServerConfiguration/GameServerConfigurationsDisplay/GameServerConfigurationsDisplay.tsx";
+    from "@components/display/GameServerConfiguration/GameServerConfigurationsDisplay/GameServerConfigurationsDisplay.tsx";
 import {useTypedSelector} from "@/stores/rootReducer.ts";
+import bgImage from "@/assets/background.png"
 
 export const Route = createFileRoute("/")({
-  component: Index,
+    component: Index,
 });
 
 function Index() {
-  const gameServers = useTypedSelector(
-    state => state.gameServerConfigurationSliceReducer.data
-  );
+    const gameServers = useTypedSelector(
+        state => state.gameServerConfigurationSliceReducer.data
+    );
 
-  return (
-    <div
-      className="
-      h-screen
-      w-screen
-      flex
-      flex-row
-      justify-center
-      items-center
-    "
-    >
-      <GameServerConfigurationsDisplay gameServerConfigurations={gameServers}/>
-    </div>
-  );
+    return (
+        <div
+            className="
+                  h-screen
+                  w-screen
+                  flex
+                  flex-row
+                  justify-center
+                  items-center
+            "
+
+            style={{
+                backgroundImage: bgImage ? `url(${bgImage})` : undefined,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+            }}
+        >
+            <GameServerConfigurationsDisplay gameServerConfigurations={gameServers}/>
+        </div>
+    );
 }
 
 export default Index;
